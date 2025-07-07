5938985fa278febef4ffd299a3c58a8d447459a3
ECommerce <priti16maurya@gmail.com>
2025-07-07 22:30:40 +0530

    Basic Go Lang

---
+++ 02variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Variables Learning tutorials")
+	var username string = "Priti"
+	fmt.Println("My Username:", username)
+	fmt.Printf("Username is type: %T \n", username)
+}
